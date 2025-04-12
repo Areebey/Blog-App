c1cd49f08b35c3ca855f96aa46872efa5e5efe55
areebey <areebrafiq125@gmail.com>
2025-04-12 17:16:07 +0500

    Pages and routes completed.

--- src/components/index.js
+++ src/components/index.js
@@ -2,24 +2,32 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "./container/Container.jsx";
 import Logo from "./Logo.jsx";
+import Button from "./Button.jsx";
+import Input from "./input.jsx";
 import LogoutBtn from "./Header/LogoutBtn.jsx";
 import RTE from "./RTE.jsx";
 import Signup from "./Signup.jsx";
-import Signin from "Signin.jsx";
-import Post from ".Post";
-import PostForm from "post-form/PostForm"
-import PostCard from "PostCard.jsx";
+import Login from "./Login.jsx";
+// import Post from "./Post";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard.jsx";
+import AuthLayout from "./AuthLayout.jsx";
+import Select from "./Select.jsx"
 
 export {
     Header,
     Footer,
     Container,
     Logo,
+    Button,
+    Input,
     LogoutBtn,
     RTE,
     Signup,
-    Signin,
-    Post,
+    Login,
+    // Post,
     PostForm,
-    PostCard
+    PostCard,
+    AuthLayout,
+    Select
 }
